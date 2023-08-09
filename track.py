5938d8e52adc8bd8ad21bfb31201b82f9c036eec
Dmytro <dima.pashko@gmail.com>
2023-08-09 12:16:08 -0400

    Minor improvements

--- track.py
+++ track.py
@@ -12,7 +12,7 @@ import os
 import sys
 
 
-def draw_save_map(points, idx):
+def draw_map_save_html(points, idx):
     base_map = folium.Map(points[idx], zoom_start=15)
     track_layer = folium.FeatureGroup(name='Track')
 
@@ -20,7 +20,7 @@ def draw_save_map(points, idx):
 
     folium.PolyLine(points, color='darkorange').add_to(track_layer)
 
-    folium.Marker(location=points[idx], icon=folium.Icon(icon="asterisk", color="blue")).add_to(track_layer)
+    folium.CircleMarker(location=points[idx], radius=7, color="blue", fill_color='yellow').add_to(track_layer)
 
     html_file = os.path.join('htmls', 'frame' + str(idx) + '.html')
 
@@ -28,40 +28,46 @@ def draw_save_map(points, idx):
     return os.path.abspath(html_file)
 
 
-if __name__ == '__main__':
-    videoFile = ''
+def read_waypoints(filename):
+    pts = []
 
-    if len(sys.argv) < 2:
-        print("Usage: " + sys.argv[0] + " input file name")
-        exit(1)
-    else:
-        videoFile = sys.argv[1]
-        print("Processing file: " + videoFile)
-
-    waypoints = []
-
-    result = subprocess.Popen(["exiftool", "-ee", "-n", "-p", "$gpslatitude,$gpslongitude", videoFile],
+    result = subprocess.Popen(["exiftool", "-ee", "-n", "-p", "$gpslatitude,$gpslongitude", filename],
                               stdout=subprocess.PIPE)
 
     for line in io.TextIOWrapper(result.stdout, encoding="utf-8"):
         coord_line = line.strip().split(",")
-        waypoints.append([float(coord_line[0]), float(coord_line[1])])
+        pts.append([float(coord_line[0]), float(coord_line[1])])
 
-    waypoints.pop(0)
-    print('Found ' + str(len(waypoints)) + ' frames')
+    pts.pop(0)
+    print('Found {} frames'.format(len(pts)))
+    return pts
+
+
+if __name__ == '__main__':
+    videoFile = ''
+
+    if len(sys.argv) < 2:
+        print("Usage: {} input file name".format(sys.argv[0]))
+        print("Example: {} test.MP4".format(sys.argv[0]))
+        exit(1)
+    else:
+        videoFile = sys.argv[1]
+        print("Processing file: {}".format(videoFile))
+
+    waypoints = read_waypoints(videoFile)
 
     index = 0
     browser = webdriver.Firefox()
 
     for x in waypoints:
         pngFile = os.path.join('screens', 'frame' + str(index) + '.png')
-        htmlFile = draw_save_map(waypoints, index)
+        htmlFile = draw_map_save_html(waypoints, index)
         browser.get('file://' + htmlFile)
         time.sleep(1)
         browser.save_screenshot(pngFile)
-        print(pngFile + ' - saved')
+        print('{} - saved'.format(pngFile))
         index += 1
 
     browser.quit()
 
-    print("Done")
+    print("Processing of {} is finished".format(videoFile))
